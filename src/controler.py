c5bedfb16b3d7857dced7c4023092023c1df93fb
JulianKrauze <150268679+JulianKrauze@users.noreply.github.com>
2026-05-27 13:59:09 -0400

    Update controler.py

--- src/controler.py
+++ src/controler.py
@@ -21,7 +21,7 @@ def shaping(download_mbps, upload_mbps, qdisc, lattency = 50, qsize = 0, r2q=100
         f"tc qdisc del dev veth2 root 2>/dev/null || true && "
         f"tc qdisc add dev veth2 root handle 1: htb default 10 r2q {r2q} && "
         f"tc class add dev veth2 parent 1: classid 1:10 htb "
-        f"rate {download_mbps}Mbit ceil {download_mbps}Mbit && "
+        f"rate {download_mbps}Mbit ceil {download_mbps}Mbit "
     )
     run_cmd(
         f"tc qdisc add dev veth2 parent 1:10 {qdisc} "
